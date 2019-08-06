73fff66d4f7ec149ca197d496b318d5f9c2c8c5a
Jason Adams <jason.the.adams@gmail.com>
2019-08-06 14:26:26 -0700

    feature: add app config with version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,8 @@ $app->withFacades();
 
 $app->withEloquent();
 
+$app->configure('app');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
