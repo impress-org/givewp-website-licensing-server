c79ffc4e78588a04c9e3db52b87b027ba968645a
Jason Adams <jason.the.adams@gmail.com>
2019-08-06 14:26:44 -0700

    feature: add app:version command for CircleCI use

---
+++ app/Console/Commands/AppVersion.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AppVersion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:version {--plain : Outputs only the app version with no new line}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Displays the current app and database versions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @since 0.1.0
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if ($this->option('plain')) {
+            $this->output->write(config('app.version'));
+        } else {
+            $this->info('App Version: ' . config('app.version'));
+        }
+    }
+}
