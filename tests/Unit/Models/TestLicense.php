4dc4943278a82aa8968e2b94fac9d53f7c9ba924
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-08-12 17:29:21 +0530

    chore: add test for model license

---
+++ tests/Unit/Models/TestLicense.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Unit\License;
+
+use App\Models\License;
+use App\Repositories\Licenses;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class TestLicense extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @var Licenses $license
+     * @access private
+     */
+    private $license;
+
+    function setUp(): void
+    {
+        parent::setUp();
+
+        $this->license = new Licenses();
+    }
+
+    /**
+     * @covers \App\Models\License::store
+     */
+    public function testStore()
+    {
+        $license_key = 'abc';
+
+        /**
+         * Case: if license does not exist
+         */
+        /* @var License|null $output */
+        $output = $this->license->get($license_key);
+
+        $this->assertEquals(null, $output);
+
+        /**
+         * Case: If license exist
+         */
+        License::store($license_key, ['dummy data']);
+
+        $output = $this->license->get($license_key);
+
+        $this->assertInstanceOf(License::class, $output);
+        $this->assertEquals($license_key, $output->license);
+        $this->assertEquals(array('dummy data'), $output->data);
+    }
+}
