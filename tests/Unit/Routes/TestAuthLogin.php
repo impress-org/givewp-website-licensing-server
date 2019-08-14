b7aff64301a75aed9416e4007bb6d5a015306f8f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-08-14 18:01:10 +0530

    chore: add test for auth/login route

---
+++ tests/Unit/Routes/TestAuthLogin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Test\Unit\Routes;
+
+use Tests\TestCase;
+
+/**
+ * Class TestAuthLogin
+ * @package Test\Unit\Routes
+ */
+class TestAuthLogin extends TestCase
+{
+    public function testShouldReturnToken(): void
+    {
+        $this->json('POST', 'auth/login', ['email' => 'xyz@test.com', 'password' => '12345' ])
+            ->seeStatusCode(200)
+            ->seeJsonStructure(['token']);
+    }
+
+    public function testShouldReturnUserNotFoundError(): void
+    {
+        $this->json('POST', 'auth/login', ['email' => 'abs@test.com', 'password' => '12345' ])
+             ->seeStatusCode(400)
+             ->seeJsonContains(['msg'=>'User does not exist.']);
+    }
+
+    public function testShouldReturnInvalidPasswordInputError(): void
+    {
+        $this->json('POST', 'auth/login', ['email' => 'abs@test.com', 'password' => '' ])
+             ->seeStatusCode(422)
+             ->seeJsonContains(['password'=>['validation.required']]);
+    }
+
+    public function testShouldReturnInvalidUserInputError(): void
+    {
+        $this->json('POST', 'auth/login', ['email' => '', 'password' => '12345' ])
+             ->seeStatusCode(422)
+             ->seeJsonContains(['email'=>['validation.required']]);
+    }
+
+    public function testShouldReturnInvalidUserOrPasswordError(): void
+    {
+        $this->json('POST', 'auth/login', ['email' => 'xyz@test.com', 'password' => '1234' ])
+             ->seeStatusCode(400)
+             ->seeJsonContains(['msg'=>'Email or password is wrong.']);
+    }
+}
