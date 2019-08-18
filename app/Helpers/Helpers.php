2edfd0afefbabe6e7c5b3a921c44a05529776465
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-08-18 11:38:00 +0530

    feat: add helper functions

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Request;
+
+if (! function_exists('getLicenseIdentifier')) {
+    /**
+     * Get unique identifier to identify license in licenses table
+     *
+     * @param  string  $license_key  License Key.
+     * @param  string  $website_url
+     *
+     * @return string
+     */
+    function getLicenseIdentifier($license_key, $website_url)
+    {
+        return substr(md5("{$license_key}|{$website_url}"), 0, 15);
+    }
+}
+
+if (! function_exists('getClientWebsiteURLFromUserAgent')) {
+    /**
+     * Get client website URL from user agent
+     *
+     * @return string
+     */
+    function getClientWebsiteURLFromUserAgent()
+    {
+        $domain = array_map('trim', explode(';', $_SERVER['HTTP_USER_AGENT']));
+        $domain = ! empty($domain[1]) ? trim($domain[1]) : '';
+
+        return $domain;
+    }
+}
