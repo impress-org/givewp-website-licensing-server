73e7a79f7d08ece9b1ef8d58c12e1a981f1b11cd
Jason Adams <jason.the.adams@gmail.com>
2019-08-26 09:54:11 -0700

    chore: bump version to 0.2.0-beta.1

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | framework needs to run upgrades or provide a notification.
     |
     */
-    'version' => '0.1.0',
+    'version' => '0.2.0-beta.1',
 
     /*
    |--------------------------------------------------------------------------
